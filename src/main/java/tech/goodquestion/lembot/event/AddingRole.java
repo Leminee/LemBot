0bed762e1a995e534cab483c209e53f69a88ca2d
Lemine <lemine.el.agheb1994@gmail.com>
2021-11-17 03:46:48 +0100

    updated ag addingRole

--- src/main/java/tech/goodquestion/lembot/event/AddingRole.java
+++ src/main/java/tech/goodquestion/lembot/event/AddingRole.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -37,21 +38,31 @@ public class AddingRole extends ListenerAdapter {
         Guild guild = member.getGuild();
         Role codingRole = guild.getRoleById(784773593942327297L);
         Role hackingRole = guild.getRoleById(811741950092116038L);
+        Role mutedRole = guild.getRoleById(879329567947489352L);
+        List<Role> memberRoles = member.getRoles();
 
-        for (Role role : member.getRoles()) {
+
+        for (Role role : memberRoles) {
 
             assert codingRole != null;
             assert hackingRole != null;
-            if (role.getIdLong() == codingRole.getIdLong() || role.getIdLong() == hackingRole.getIdLong()) {
+            assert mutedRole != null;
+
+            boolean hasAlreadyAccess = role.getIdLong() == codingRole.getIdLong() || role.getIdLong() == hackingRole.getIdLong();
+            boolean isMuted = role.getIdLong() == mutedRole.getIdLong();
+
+            if (hasAlreadyAccess || isMuted) {
 
                 ScheduledFuture<?> task = tasks.remove(member.getIdLong());
                 if (task != null) task.cancel(false);
                 return;
             }
         }
+
         assert codingRole != null;
-        guild.addRoleToMember(member,codingRole).queue();
+        guild.addRoleToMember(member, codingRole).queue();
         ScheduledFuture<?> task = tasks.remove(member.getIdLong());
         if (task != null) task.cancel(false);
     }
+
 }
