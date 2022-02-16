45cc744a813f6b3b0f97f5a7f441c3cd2448de86
Lemine <lemine.el.agheb1994@gmail.com>
2022-02-16 03:34:03 +0100

    added inform when killing the bot

--- src/main/java/tech/goodquestion/lembot/command/impl/KillSwitchCommand.java
+++ src/main/java/tech/goodquestion/lembot/command/impl/KillSwitchCommand.java
@@ -4,13 +4,22 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import tech.goodquestion.lembot.command.IBotCommand;
+import tech.goodquestion.lembot.config.Config;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public final class KillSwitchCommand implements IBotCommand {
 
     @Override
     public void dispatch(final Message message, final TextChannel channel, final Member sender, final String[] args) throws IOException {
+
+
+        Objects.requireNonNull(Config.getInstance()
+                .getGuild()
+                .getTextChannelById(Config.getInstance().getChannelConfig().getStaffRoomChannel().getIdLong()))
+                .sendMessage(Objects.requireNonNull(Config.getInstance().getGuild().getOwner()).getAsMention() + "Bot wurde durch " + sender.getAsMention() + "gekillt")
+                .queue();
 
         System.exit(0);
     }
