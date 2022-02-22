1797bfac6d9fbbbeb5f481680df187f3033a533c
Lemine <lemine.el.agheb1994@gmail.com>
2022-02-22 01:11:19 +0100

    updated MediaOnly to MemesOnly

--- src/main/java/tech/goodquestion/lembot/event/MemesOnlyChannel.java
+++ src/main/java/tech/goodquestion/lembot/event/MemesOnlyChannel.java
@@ -1,34 +1,54 @@
 package tech.goodquestion.lembot.event;
 
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import tech.goodquestion.lembot.config.Config;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public final class MediaOnly extends ListenerAdapter {
+public final class MemesOnlyChannel extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(final MessageReceivedEvent event) {
 
         final long memeChannelId = Config.getInstance().getChannelConfig().getMemesChannel().getIdLong();
-        final Set<Long> mediaOnlyChannels = Set.of(
+        final String messageContent = event.getMessage().getContentRaw();
+        final Message message = event.getMessage();
+        final Set<Long> memesOnlyChannels = Set.of(
                 memeChannelId
         );
 
         final long currentChannelId = event.getChannel().getIdLong();
 
-        if (mediaOnlyChannels.contains(currentChannelId) && !messageContainsMedia(event)) deleteTextMessage(event);
+        if (!memesOnlyChannels.contains(currentChannelId)) return;
+
+        if (!containsMedia(message) && !containsOnlyURL(messageContent)) deleteTextMessage(message);
+
+        event.getMessage()
+                .reply("In diesem Kanal dürfen nur Memes (als Media-Dateien oder Links) gepostet werden")
+                .queue(m -> m.delete().queueAfter(30, TimeUnit.SECONDS));
 
     }
 
-    private boolean messageContainsMedia(final MessageReceivedEvent event) {
+    private boolean containsMedia(final Message message) {
 
-        return event.getMessage().getAttachments().size() != 0;
+        return message.getAttachments().size() != 0;
     }
 
-    private void deleteTextMessage(final MessageReceivedEvent event) {
+    private boolean containsOnlyURL(final String messageContent) {
 
-        event.getMessage().delete().queue();
+        Pattern pattern = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+        Matcher matcher = pattern.matcher(messageContent);
+
+        return matcher.matches();
+    }
+
+    private void deleteTextMessage(final Message message) {
+
+        message.delete().queue();
     }
 }
