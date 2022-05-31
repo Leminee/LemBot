eb56b8488bf599299b34d32a1bdd9814a489777d
Lemine <lemine.el.agheb1994@gmail.com>
2022-05-31 05:03:33 +0200

    added new keywords to array

--- src/main/java/tech/goodquestion/lembot/event/CodeBlockChecker.java
+++ src/main/java/tech/goodquestion/lembot/event/CodeBlockChecker.java
@@ -9,55 +9,150 @@ import java.util.concurrent.TimeUnit;
 public final class CodeBlockChecker extends ListenerAdapter {
 
     private static final String[] KEYWORDS = {
-            "from",
+            "auto",
             "break",
             "case",
-            "switch",
-            "int",
-            "String",
-            "class",
+            "char",
+            "const",
             "continue",
             "default",
             "do",
             "double",
             "else",
             "enum",
-            "extends",
-            "final",
-            "finally",
+            "extern",
             "float",
             "for",
             "goto",
             "if",
-            "implements",
-            "import",
-            "instanceof",
-            "interface",
+            "int",
             "long",
-            "native",
-            "new",
-            "package",
-            "private",
-            "protected",
-            "public",
+            "register",
             "return",
             "short",
+            "signed",
+            "sizeof",
             "static",
-            "strictfp",
-            "super",
+            "struct",
             "switch",
-            "synchronized",
-            "this",
-            "throw",
-            "throws",
-            "transient",
-            "try",
+            "typedef",
+            "union",
+            "unsigned",
             "void",
             "volatile",
             "while",
-            "true",
-            "false",
+            "_Packed",
+            "abstract",
+            "byte",
+            "class",
+            "extends",
+            "import",
+            "private",
+            "throws",
+            "assert",
+            "final",
+            "instanceof",
+            "native",
+            "protected",
+            "synchronized",
+            "transient",
+            "while",
+            "boolean",
+            "and",
+            "catch",
+            "finally",
+            "this",
+            "try",
+            "char",
+            "implements",
+            "interface",
+            "package",
+            "def",
+            "elif",
+            "except",
+            "exec",
+            "from",
+            "global",
+            "is",
+            "in",
+            "lambda",
+            "not",
+            "pass",
+            "or",
+            "print",
+            "raise",
+            "yield",
+            "val",
+            "var",
+            "let",
+            "typeof",
             "null",
+            "function",
+            "func",
+            "foreach",
+            "explicit",
+            "sealed",
+            "ulong",
+            "unchecked",
+            "base",
+            "checked",
+            "unsafe",
+            "readonly",
+            "operator",
+            "fixed",
+            "out",
+            "ref",
+            "decimal",
+            "uint",
+            "<head>",
+            "<title>",
+            "<body>",
+            "<html>",
+            "<meta>",
+            "<link>",
+            "<style>",
+            "<script>",
+            "<div>",
+            "<span>",
+            "<br>",
+            "<hr>",
+            "<h1>",
+            "<h2>",
+            "<h3>",
+            "<h4>",
+            "<h5>",
+            "<h6>",
+            "<p>",
+            "<a>",
+            "<i>",
+            "<u>",
+            "<strong>",
+            "<em>",
+            "<ol>",
+            "<ul>",
+            "<li>",
+            "<dl>",
+            "<dt>",
+            "<dd>",
+            "<table>",
+            "<tr>",
+            "<th>",
+            "<td>",
+            "<tbody>",
+            "<thead>",
+            "<tfoot>",
+            "<col>",
+            "<colgroup>",
+            "<caption>",
+            "<form>",
+            "<input>",
+            "<button>",
+            "<select>",
+            "<option>",
+            "<textarea>",
+            "<label>",
+            "<fieldset>",
+            "<legend>"
     };
 
     @Override
@@ -88,34 +183,27 @@ public final class CodeBlockChecker extends ListenerAdapter {
 
         int probability = 0;
 
-        final String[] splitByWitheSpace = messageContent.split(" ");
+        final String[] words = messageContent.split(" ");
+        final String[] lines = messageContent.split("\n");
 
-        final String[] splitByLine = messageContent.split("\n");
-
-        for (final String word : splitByWitheSpace) {
+        for (final String word : words) {
 
             for (final String keyword : KEYWORDS) {
 
-                if (word.equalsIgnoreCase(keyword)) {
-                    probability += 10;
-                }
+                if (word.equalsIgnoreCase(keyword)) probability += 5;
             }
         }
 
-        for (final String line : splitByLine) {
+        for (final String line : lines) {
 
-            if (line.startsWith(" ")) {
+            if (line.startsWith(" ")) probability += 5;
 
-                probability += 10;
-            }
+            if (line.contains("{") || line.contains("}")) probability += 5;
 
-
-            if (line.contains("{") || line.contains("}")) {
-                probability += 10;
-            }
+            if (line.contains("=") || line.contains("()") || line.contains(":")) probability += 5;
         }
 
-        return probability >= 90;
+        return probability >= 95;
     }
 
     private boolean containsCodeBlock(final String messageContent) {
