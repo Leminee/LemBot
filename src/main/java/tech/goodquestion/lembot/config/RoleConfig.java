d4dc839832010998e63a79f81b56b1c8f7560c79
Leminee <lemine.el.agheb1994@gmail.com>
2025-07-19 20:27:00 +0200

    removed roles

--- src/main/java/tech/goodquestion/lembot/config/RoleConfig.java
+++ src/main/java/tech/goodquestion/lembot/config/RoleConfig.java
@@ -5,26 +5,12 @@ import net.dv8tion.jda.api.entities.Role;
 public final class RoleConfig {
 
     private long bump;
-    private long mute;
-    private long warn;
     private long coding;
     private long moderator;
     private long admin;
 
     public long getBumpRoleId() {
         return bump;
-    }
-
-    public Role getCodingRole() {
-        return Config.getInstance().getGuild().getRoleById(coding);
-    }
-
-    public Role getMuteRole() {
-        return Config.getInstance().getGuild().getRoleById(mute);
-    }
-
-    public Role getWarnRole() {
-        return Config.getInstance().getGuild().getRoleById(warn);
     }
 
     public Role getModeratorRole() {
