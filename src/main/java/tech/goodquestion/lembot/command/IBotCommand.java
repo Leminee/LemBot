0726b42873ac822f28208e45db51ce51e4f46e26
Leminee <lemine.el.agheb1994@gmail.com>
2025-07-19 20:26:20 +0200

    updated permissions

--- src/main/java/tech/goodquestion/lembot/command/IBotCommand.java
+++ src/main/java/tech/goodquestion/lembot/command/IBotCommand.java
@@ -1,9 +1,9 @@
 package tech.goodquestion.lembot.command;
 
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
+import tech.goodquestion.lembot.library.Helper;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -18,7 +18,7 @@ public interface IBotCommand {
     String getDescription();
 
     default boolean isPermitted(final Member member) {
-        return member.getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase("administrator")) || member.getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase("moderator"));
+        return Helper.isStaff(member);
     }
 
     default String getHelpList() {
